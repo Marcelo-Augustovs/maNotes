926d7659b4ee3e38a266a21588c8f18d78d1835c
Marcelo Augusto <marcelo.augustovs99@gmail.com>
2024-10-18 22:00:30 -0300

    Criado FundosEDespesas(Repository e Service)

---
+++ src/main/java/dev_marcelo/maNotes/repository/FundosEDespesasRepository.java
@@ -0,0 +1,7 @@
+package dev_marcelo.maNotes.repository;
+
+import dev_marcelo.maNotes.entity.FundosEDespesas;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FundosEDespesasRepository extends JpaRepository<FundosEDespesas,Float> {
+}
---
+++ src/main/java/dev_marcelo/maNotes/service/FundosEDespesasService.java
@@ -0,0 +1,9 @@
+package dev_marcelo.maNotes.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class FundosEDespesasService {
+}
