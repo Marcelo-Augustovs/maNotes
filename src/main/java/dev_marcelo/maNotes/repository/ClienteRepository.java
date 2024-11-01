3c7b03c665e271b1a0a46c4ab3862e4876c17eaf
Marcelo Augusto <marcelo.augustovs99@gmail.com>
2024-11-01 16:08:38 -0300

    Removido a entity cliente, a funcionalidade foi para o usuario

--- src/main/java/dev_marcelo/maNotes/repository/ClienteRepository.java
+++
@@ -1,8 +0,0 @@
-package dev_marcelo.maNotes.repository;
-
-import dev_marcelo.maNotes.entity.Cliente;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ClienteRepository extends JpaRepository<Cliente,String> {
-
-}
