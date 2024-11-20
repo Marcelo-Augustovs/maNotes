4841cedb3d59b3052077277a321f6fc80b142eb2
Marcelo Augusto <marcelo.augustovs99@gmail.com>
2024-11-20 15:40:12 -0300

    configuracao e implementacao de auditoria

---
+++ src/main/java/dev_marcelo/maNotes/config/SpringJpaAuditingConfig.java
@@ -0,0 +1,17 @@
+package dev_marcelo.maNotes.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import java.util.Optional;
+
+@EnableJpaAuditing
+@Configuration
+public class SpringJpaAuditingConfig implements AuditorAware<String> {
+
+    @Override
+    public Optional<String> getCurrentAuditor() {
+        return Optional.empty();
+    }
+}
--- src/main/java/dev_marcelo/maNotes/dto/AnotacoesResponseDto.java
+++ src/main/java/dev_marcelo/maNotes/dto/AnotacoesResponseDto.java
@@ -1,15 +1,18 @@
 package dev_marcelo.maNotes.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import java.time.LocalDateTime;
+import java.time.LocalDate;
 
 @AllArgsConstructor @NoArgsConstructor @Getter @Setter
 public class AnotacoesResponseDto {
     private Float id;
     private String anotacao;
-    private LocalDateTime dataModificacao;
+    @JsonFormat(pattern = "dd-MM-yyyy")
+    private LocalDate dataModificacao;
 }
