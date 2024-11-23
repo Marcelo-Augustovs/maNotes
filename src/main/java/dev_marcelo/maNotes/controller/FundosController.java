9065625d63d35d5c8b624331105336f5b32644c6
Marcelo Augusto <marcelo.augustovs99@gmail.com>
2024-11-23 20:30:04 -0300

    Criado CRUD para fundos

---
+++ src/main/java/dev_marcelo/maNotes/controller/FundosController.java
@@ -0,0 +1,47 @@
+package dev_marcelo.maNotes.controller;
+
+
+import dev_marcelo.maNotes.dto.FundosValoresDto;
+import dev_marcelo.maNotes.entity.Fundos;
+import dev_marcelo.maNotes.service.FundosService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("api/v1/fundos")
+public class FundosController {
+    private final FundosService fundosService;
+
+    @PostMapping
+    public ResponseEntity<Fundos> create(@RequestBody Fundos fundos){
+        fundosService.save(fundos);
+        return ResponseEntity.status(201).body(fundos);
+    }
+
+    @PatchMapping("/{id}")
+    public ResponseEntity<Fundos> updateValores(@PathVariable Float id, @RequestBody FundosValoresDto dto){
+        Fundos fundos = fundosService.updateValor(id,dto.getOrigemDoFundo(),dto.getValorRecebido());
+        return ResponseEntity.ok().body(fundos);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Fundos> findFundos(@PathVariable Float id){
+        return ResponseEntity.ok().body(fundosService.findById(id));
+    }
+
+    @GetMapping()
+    public ResponseEntity<List<Fundos>> findAllFundos(){
+        return ResponseEntity.ok().body(fundosService.findAllFundos());
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteFundos(@PathVariable Float id){
+        fundosService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
+}
---
+++ src/main/java/dev_marcelo/maNotes/service/FundosService.java
@@ -0,0 +1,49 @@
+package dev_marcelo.maNotes.service;
+
+import dev_marcelo.maNotes.entity.Fundos;
+import dev_marcelo.maNotes.repository.FundosRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Service
+public class FundosService {
+    private final FundosRepository fundosRepository;
+
+    @Transactional
+    public Fundos save(Fundos fundos){
+        return fundosRepository.save(fundos);
+    }
+
+    @Transactional
+    public Fundos updateValor(Float id,String origemDoFundo,Float valorRecebido){
+        Fundos fundosAtualizados = fundosRepository.findById(id)
+                .orElseThrow(
+                () -> new RuntimeException("Fundo não encontrado"));
+        if(fundosAtualizados != null) fundosAtualizados.setOrigemDoFundo(origemDoFundo);
+        fundosAtualizados.setValorRecebido(valorRecebido);
+        return fundosAtualizados;
+    }
+
+    @Transactional(readOnly = true)
+    public Fundos findById(Float id){
+        return fundosRepository.findById(id)
+                .orElseThrow( () -> new RuntimeException(String.format("fundo id:%s não encontrado",id)));
+    }
+
+    @Transactional(readOnly = true)
+    public List<Fundos> findAllFundos(){
+        return fundosRepository.findAll();
+    }
+
+    @Transactional
+    public void delete(Float id){
+        Fundos fundos = fundosRepository.findById(id).orElseThrow(
+                () -> new RuntimeException(String.format("fundo id:%s não encontrado",id))
+        );
+        fundosRepository.delete(fundos);
+    }
+}
