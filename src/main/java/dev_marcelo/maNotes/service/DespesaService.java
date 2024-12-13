61b3e8af6fed750e0232d318bdc9d1d8b1843381
Marcelo Augusto <marcelo.augustovs99@gmail.com>
2024-12-12 21:03:18 -0300

    Adicionado o metodo Update

--- src/main/java/dev_marcelo/maNotes/service/DespesaService.java
+++ src/main/java/dev_marcelo/maNotes/service/DespesaService.java
@@ -1,5 +1,6 @@
 package dev_marcelo.maNotes.service;
 
+import dev_marcelo.maNotes.dto.DespesaDto;
 import dev_marcelo.maNotes.entity.Despesa;
 import dev_marcelo.maNotes.repository.DespesaRepository;
 import lombok.RequiredArgsConstructor;
@@ -51,5 +52,23 @@ public class DespesaService {
     @Transactional
     public void delete(Despesa despesa) {
         despesaRepository.delete(despesa);
+    }
+
+    @Transactional
+    public Despesa updateDespesa(Float id, DespesaDto dto) {
+      Despesa despesa = despesaRepository.findById(id).orElseThrow(
+              () -> new RuntimeException("despesa não encontrada")
+      );
+
+      despesa.setNomeDaConta(
+        (dto.getNomeDaConta() == null || dto.getNomeDaConta().isEmpty())
+            ? despesa.getNomeDaConta()
+            : dto.getNomeDaConta() );
+
+        despesa.setValorDaConta(
+                (dto.getValorDaConta() == null || dto.getValorDaConta() < 0)
+                        ? despesa.getValorDaConta()
+                        : dto.getValorDaConta() );
+        return despesa;
     }
 }
