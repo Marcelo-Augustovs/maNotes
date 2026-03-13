99272503c06cf277e733d8c7b1a5c252ad48a79a
Marcelo Augusto <marcelo.augustovs99@gmail.com>
2026-03-13 17:48:55 -0300

    renamed

--- src/main/java/dev_marcelo/maNotes/dto/fundos_despesa/BalancoMensalDto.java
+++ src/main/java/dev_marcelo/maNotes/dto/fundos_despesa/BalancoMensalDto.java
@@ -1,4 +1,4 @@
 package dev_marcelo.maNotes.dto.fundos_despesa;
 
-public record BalancoMensal(int mes, double totalFundos, double totalDespesas, double ganhoDoMes) {
+public record BalancoMensalDto(int mes, double totalFundos, double totalDespesas, double ganhoDoMes) {
 }
