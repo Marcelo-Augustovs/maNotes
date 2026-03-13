99e1e0524e71c6a4649cc6a1f2266d77a26dcc40
Marcelo Augusto <marcelo.augustovs99@gmail.com>
2026-03-13 17:46:13 -0300

    Adicionado metodo para otimizacao em alta escala

--- src/main/java/dev_marcelo/maNotes/repository/DespesaRepository.java
+++ src/main/java/dev_marcelo/maNotes/repository/DespesaRepository.java
@@ -1,11 +1,17 @@
 package dev_marcelo.maNotes.repository;
 
 import dev_marcelo.maNotes.entity.Despesa;
+import dev_marcelo.maNotes.entity.Fundos;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.time.LocalDate;
+import java.util.List;
 
 public interface DespesaRepository extends JpaRepository<Despesa,Long> {
 
     Despesa findByNomeDaConta(String nomeDaConta);
 
     Despesa findByValorDaConta(Float valorDaConta);
+
+    List<Despesa> findByDataModificacaoBetween(LocalDate start, LocalDate end);
 }
--- src/main/java/dev_marcelo/maNotes/repository/FundosRepository.java
+++ src/main/java/dev_marcelo/maNotes/repository/FundosRepository.java
@@ -3,6 +3,10 @@ package dev_marcelo.maNotes.repository;
 import dev_marcelo.maNotes.entity.Fundos;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
+import java.util.List;
+
 public interface FundosRepository extends JpaRepository<Fundos,Long> {
     Fundos findByOrigemDoFundo(String fundos);
+    List<Fundos> findByDataModificacaoBetween(LocalDate start, LocalDate end);
 }
